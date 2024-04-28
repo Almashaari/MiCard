fa4c19de675894af835d175fd7a8423dfbaea79c
AnwarNa <anwar.se19@gmail.com>
2024-04-28 11:47:22 +0300

    Add box info

--- lib/main.dart
+++ lib/main.dart
@@ -9,35 +9,80 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.white,
         body: SafeArea(
+          child: Center(
             child: Column(
-          children: [
-            CircleAvatar(
-              backgroundImage: AssetImage("images/profile_pic.jpg"),
-              radius: 50,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage("images/profile_pic.jpg"),
+                  radius: 50,
+                ),
+                Text(
+                  'Anwar Nabil',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Flutter Developer',
+                  style: TextStyle(
+                    fontFamily: 'SourceSans',
+                    fontSize: 20,
+                    color: Colors.black,
+                    letterSpacing: 3,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 200,
+                  child: Divider(color: Colors.black),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.black,
+                    ),
+                    title: Text(
+                      '+974 7007 0985',
+                      style: TextStyle(
+                        fontFamily: 'SourceSans',
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.black,
+                    ),
+                    title: Text(
+                      'anwar.se19@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'SourceSans',
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'Anwar Nabil',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              'Flutter Developer',
-              style: TextStyle(
-                fontFamily: 'SourceSans',
-                fontSize: 20,
-                color: Colors.white,
-                letterSpacing: 3,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        )),
+          ),
+        ),
       ),
     );
   }
